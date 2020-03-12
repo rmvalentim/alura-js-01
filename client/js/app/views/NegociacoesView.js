d261d3e8c0a63c784f30073fe73c710d56450ecc
Rafael Valentim <rmoraesvalentim@gmail.com>
2020-03-12 11:57:21 -0300

    Finalizada aula 6 e fechando o curso 1

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,11 +1,6 @@
-class NegociacoesView {
+class NegociacoesView extends View {
 
-    constructor ( elemento ) {
-
-        this._elemento = elemento;
-    }
-
-    _template ( model ) {
+    template ( model ) {
         return `
         <table class="table table-hover table-bordered">
             <thead>
@@ -40,11 +35,6 @@ class NegociacoesView {
             </tfoot>
         </table>
         `;
-    }
-
-    update ( model ) {
-
-        this._elemento.innerHTML = this._template( model );
     }
 }
 
