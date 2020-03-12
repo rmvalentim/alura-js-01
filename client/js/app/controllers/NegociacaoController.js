439d7b624c413837791860b067b4f41fb550e9b2
Rafael Valentim <rmoraesvalentim@gmail.com>
2020-03-12 16:45:01 -0300

    Escopo do this em function utilizando API de Reflection do JS

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -8,7 +8,10 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(this, function( model ) {
+   
+            this._negociacoesView.update( model );
+        });
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
 
@@ -21,12 +24,17 @@ class NegociacaoController {
 
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
-
         this._mensagem.texto = 'Negociação adicionada com sucesso';
         this._mensagemView.update(this._mensagem);    
 
         this._limpaFormulario();        
+    }
+
+    apaga () {
+
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        this._mensagemView.update(this._mensagem);    
     }
 
     _criaNegociacao () {
