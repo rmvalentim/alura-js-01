c425b5b204764057173da02cc526fd21640d195a
Rafael Valentim <rmoraesvalentim@gmail.com>
2020-03-20 15:45:52 -0300

    Utilizando Promise do HttpService no NegociacaoService

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -7,73 +7,47 @@ class NegociacaoService {
 
     obterNegociacoesDaSemana() {
 
-        return new Promise((resolve, reject) => {
-
-            this._http
-                .get('negociacoes/semana')
-                .then(negociacoes => {
-                    console.log(negociacoes);
-                    resolve(negociacoes.map(item => new Negociacao( new Date(item.data), item.quantidade, item.valor )))
-                })
-                .catch(erro => {                    
-                    console.log(erro);
-                    reject('Não foi possível importar as negociações da semana.');
-                });    
-        });   
+        return this._http
+            .get('negociacoes/semana')
+            .then(negociacoes => {
+                console.log(negociacoes);
+                return negociacoes.map(item => new Negociacao( new Date(item.data), item.quantidade, item.valor ))
+            })
+            .catch(erro => {                    
+                console.log(erro);
+                throw new Error('Não foi possível importar as negociações da semana.');
+            });    
+          
     }
 
     obterNegociacoesDaSemanaAnterior() {
-            
-        return new Promise((resolve, reject) => {
-
-            let xhr = new XMLHttpRequest();
-
-            xhr.open('GET', 'negociacoes/anterior');
-
-            xhr.onreadystatechange = () => {
-
-                if( xhr.readyState == 4 ) {
-                    if( xhr.status == 200 ) {
-                        
-                        resolve(JSON.parse(xhr.responseText)
-                            .map(item => new Negociacao( new Date(item.data), item.quantidade, item.valor )));
-
-                    } else {                    
-                        console.log(xhr.responseText);  
-                        reject('Não foi possível obter as negociações da semana anterior.');
-                    }
-                }
-            };
-
-            xhr.send();
-        });  
+        
+        return this._http
+            .get('negociacoes/anterior')
+            .then(negociacoes => {
+                console.log(negociacoes);
+                return negociacoes.map(item => new Negociacao( new Date(item.data), item.quantidade, item.valor ))
+            })
+            .catch(erro => {                    
+                console.log(erro);
+                throw new Error('Não foi possível importar as negociações da semana anterior.');
+            });   
+      
     }
 
     obterNegociacoesDaSemanaRetrasada() {
-            
-        return new Promise((resolve, reject) => {
 
-            let xhr = new XMLHttpRequest();
-
-            xhr.open('GET', 'negociacoes/retrasada');
-
-            xhr.onreadystatechange = () => {
-
-                if( xhr.readyState == 4 ) {
-                    if( xhr.status == 200 ) {
-                        
-                        resolve(JSON.parse(xhr.responseText)
-                            .map(item => new Negociacao( new Date(item.data), item.quantidade, item.valor )));
-
-                    } else {                    
-                        console.log(xhr.responseText);  
-                        reject('Não foi possível obter as negociações da semana retrasada.');
-                    }
-                }
-            };
-
-            xhr.send();
-        });  
+        return this._http
+            .get('negociacoes/retrasada')
+            .then(negociacoes => {
+                console.log(negociacoes);
+                return negociacoes.map(item => new Negociacao( new Date(item.data), item.quantidade, item.valor ))
+            })
+            .catch(erro => {                    
+                console.log(erro);
+                throw new Error('Não foi possível importar as negociações da semana retrasada.');
+            });   
+          
     }
 
 }
